5f7417a1981399263a29abf773ef61b795c16e79
John <johnsmilga@gmail.com>
2021-02-03 12:41:43 -0800

    show error in project 6

--- examples/6-newsletter/app.js
+++ examples/6-newsletter/app.js
@@ -11,13 +11,12 @@ form.addEventListener('submit', async function (e) {
     await axios.post('/api/6-newsletter', {
       email,
     })
-    form.classList.remove('loading')
     form.innerHTML = `<h4 class="success">Success! Please check your email</h4>`
   } catch (error) {
-    // console.log(error.response.data)
+    console.log(error.response.data)
     alert.style.display = 'block'
-    form.classList.remove('loading')
     // alert.textContent = error.response.data[0]
     alert.textContent = 'Something went wrong. Please try again'
   }
+  form.classList.remove('loading')
 })
